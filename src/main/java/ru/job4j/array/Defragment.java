2110b3b14bb207d6cbc62ed9b12e570d15e2ad73
andreybannikov <andrewnnov@yandex.ru>
2022-01-22 13:01:26 +0300

    fixed 6.8. Дефрагментация массива [#225 #252296]

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -4,10 +4,9 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int point = index;
-                for (int in  = point; in < array.length; in++) {
+                for (int in  = index; in < array.length; in++) {
                     if (array[in] != null) {
-                        array[point] = array[in];
+                        array[index] = array[in];
                         array[in] = null;
                         break;
                     }
